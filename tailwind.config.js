2b7e023d525e437587470276ccf165d99a00f67e
Roland Pákai <roland.pakai@liferay.com>
2023-03-28 19:26:32 +0200

    Add font-geomanist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend:
+    {
+      fontFamily: {
+        'geomanist': ['Geomanist', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
