7d7e43fac1f111ab8367c1d7012ef2b00bdc91b1
Roland Pákai <roland.pakai@liferay.com>
2023-03-29 14:50:48 +0200

    Merge divs

--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -12,19 +12,17 @@ const TabList: React.FC<TabListProps> = (props: TabListProps) => {
   const listData = useSelector((state : AppState) => state.brokerData[storeKey as keyof BrokerDataSlice]);
 
   return (
-    <div className="h-72">
-      <div className="pb-13">
-        {listData.map((data, index) => (
-          <ListItem
-            key={data.id}
-            index={index + 1}
-            name={data.name}
-            score={data.score}
-            linkUrl={data.linkUrl} 
-            logoUrl={data.logoUrl} 
-          />
-        ))}
-      </div>
+    <div className="pb-13 h-72">
+      {listData.map((data, index) => (
+        <ListItem
+          key={data.id}
+          index={index + 1}
+          name={data.name}
+          score={data.score}
+          linkUrl={data.linkUrl} 
+          logoUrl={data.logoUrl} 
+        />
+      ))}
     </div>
   )
 };
