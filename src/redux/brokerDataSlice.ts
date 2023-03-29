26f98917bd1728500ca9bb37b34cc461c0d713a4
Roland Pákai <roland.pakai@liferay.com>
2023-03-29 12:52:55 +0200

    Add setBrokers reducer

--- src/redux/brokerDataSlice.ts
+++ src/redux/brokerDataSlice.ts
@@ -1,13 +1,42 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import type { Broker } from "../data/brokers";
 
-type BrokerDataSlice = {
-}
+export type BrokerDataSlice = {
+    all: Broker[],
+    forex: Broker[],
+    stock: Broker[],
+};
 
 const initialState: BrokerDataSlice = {
-}
+    all: [],
+    forex: [],
+    stock: [],
+};
 
 export const brokerDataSlice = createSlice({
     name: "brokerData",
     initialState,
-    reducers: {},
-})
+    reducers: {
+        setBrokers(state, action: PayloadAction<Broker[]>) {
+            const data = action.payload;
+
+            const forex = data
+			    .filter((broker) => broker.isForex)
+			    .sort((a, b) => b?.score - a?.score);
+
+            const stock = data
+			    .filter((broker) => broker.isStock)
+			    .sort((a, b) => b?.score - a?.score);
+			
+            return {
+                ...state, 
+                all: data,
+                forex,
+                stock,
+            };
+		},
+    },
+});
+
+export const { setBrokers } = brokerDataSlice.actions;
+export default brokerDataSlice.reducer;
