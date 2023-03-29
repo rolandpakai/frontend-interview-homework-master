35185386314922b3b5b18a8399e37935eb6c8dcf
Roland Pákai <roland.pakai@liferay.com>
2023-03-29 19:43:13 +0200

    Implement Search logic

--- src/components/SearchContainer.tsx
+++ src/components/SearchContainer.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+
+import { AppState } from '../redux/store';
+import { Broker } from '../data/brokers'
 import SearchInput from './SearchInput';
 import SearchResult from './SearchResult';
 
@@ -9,11 +13,34 @@ type SearchContainerProps = {
 
 const SearchContainer: React.FC<SearchContainerProps> = (props: SearchContainerProps) => {
   const { title, placeholder } = props;
+  const stateData = useSelector((state : AppState) => state.brokerData.all);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [result, setResult] = useState<Broker[]>([]);
+
+  useEffect(() => {
+    setResult(stateData);
+  }, []);
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filteredData = stateData.filter(data => 
+        data.name.toLocaleLowerCase().startsWith(searchTerm.toLocaleLowerCase())
+      );
+      setResult(filteredData);
+    } else {
+      setResult(stateData);
+    }
+  }, [searchTerm]);
 
   return (
     <div className="px-8 pt-1 pb-16 relative">
-      <SearchInput title={title} placeholder={placeholder} />
-      <SearchResult />
+      <SearchInput 
+        title={title} 
+        placeholder={placeholder}
+        value={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+      <SearchResult data={result} />
     </div>
   )
 };
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 type SearchInputProps = { 
   title: string,
   placeholder: string,
+  value: string,
+  setSearchTerm: (value: string) => void,
 };
 
 const SearchInput: React.FC<SearchInputProps> = (props: SearchInputProps) => {
-  const { title, placeholder } = props;
+  const { title, placeholder, value, setSearchTerm } = props;
+
+  const onChangeHandler = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const clearInput = () => {
+    setSearchTerm("");
+  };
 
   return (
     <div className="">
@@ -17,13 +29,19 @@ const SearchInput: React.FC<SearchInputProps> = (props: SearchInputProps) => {
         { title }
       </label>
       <div className="flex">
-          <div className="mb-3 xl:w-96 bg-white border border-gray-700 rounded">
+          <div className="mb-3 xl:w-96 bg-white border border-gray-700 rounded flex items-center">
             <input
-              type="search"
-              className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
-              id="exampleSearch"
+              type="text"
+              className="relative m-0 block w-full min-w-0 flex-auto bg-transparent bg-clip-padding px-3 py-1.5 text-neutral-700 outline-none transition duration-300 ease-in-out"
               placeholder={placeholder}
-              />
+              onChange={(e) => onChangeHandler(e.target.value)}
+              value={value}
+            />
+            <FontAwesomeIcon 
+              icon={faXmark}
+              className="cursor-pointer inset-y-0 right-0 pr-3"
+              onClick={clearInput}
+            />
           </div>
       </div>
     </div>
