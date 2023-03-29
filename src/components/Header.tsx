7aa887255b28374b896582d4bd243bca1634554c
Roland Pákai <roland.pakai@liferay.com>
2023-03-29 14:51:08 +0200

    Add top padding to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = { title: string };
 const Header: React.FC<HeaderProps> = ({ title }: HeaderProps) => {
 
   return (
-    <header className="flex items-center px-4 py-2">
+    <header className="flex items-center px-4 pt-10">
       <strong className="mx-auto font-bold sm:text-2xl text-x">{title}</strong>
     </header>
   )
