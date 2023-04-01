fa3d562df8b9e5728a61956e4858257fa1ec40ea
Roland Pákai <roland.pakai@liferay.com>
2023-04-01 13:53:34 +0200

    Removed not used useEffect import

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
