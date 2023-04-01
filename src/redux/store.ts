968bbeeccf79bb4f0ace69daac83cb7a80e7e808
Roland Pákai <roland.pakai@liferay.com>
2023-04-01 15:51:07 +0200

    Removed not used types

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,8 +5,6 @@ import { useDispatch } from 'react-redux';
 import { brokerDataSlice } from './brokerDataSlice';
 import { eventDataSlice } from './eventDataSlice';
 
-type AppThunkExtra = {};
-
 const reducer = combineReducers({
   brokerData: brokerDataSlice.reducer,
   eventData: eventDataSlice.reducer,
@@ -25,12 +23,6 @@ export type AppStore = typeof store;
 export type AppState = ReturnType<AppStore['getState']>;
 
 export type AppDispatch = AppStore['dispatch'];
-
-export type AppAsyncThunkConfig = {
-  state: AppState;
-  dispatch: AppDispatch;
-  extra: AppThunkExtra;
-};
 
 export const useAppDispatch = () => {
   return useDispatch<AppDispatch>();
